3f5bda18a9f417219580b82735474e791854c406
takao.nakaguchi <takao.nakaguchi@cf2c226f-224e-0410-9fc3-354f046c9d04>
2009-01-05 20:27:52 +0000

    update build scripts

--- trunk/ivyrepository/install-aspectj.sh
+++ trunk/ivyrepository/install-aspectj.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
 	echo "Usage: install-aspectj.sh revision"
+	echo "*unpack distribution to work directory before executing*"
 	exit 1
 fi
-java -jar ../build/lib.ivy/ivy-2.0.0-rc2.jar -settings ivysettings.xml -ivy install-aspectj-$1.xml -publish internal -revision $1 -publishpattern work/aspectj-$1/lib/[artifact].[ext] -deliverto work/aspectj-[revision].xml
+java -jar ../build/lib.ivy/ivy-2.0.0-rc2.jar -settings ivy.settings.xml -ivy install-aspectj-$1.xml -publish internal -revision $1 -publishpattern work/aspectj-$1/lib/[artifact].[ext] -deliverto work/aspectj-[revision].xml
--- trunk/ivyrepository/install-jgroups.sh
+++ trunk/ivyrepository/install-jgroups.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ $# -ne 1 ]; then
 	echo "Usage: install-jgroups.sh revision"
+	echo "*unpack distribution to work directory before executing*"
 	exit 1
 fi
-java -jar ../build/lib.ivy/ivy-2.0.0-rc2.jar -settings ivysettings.xml -ivy install-jgroups-$1.xml -publish internal -revision $1 -publishpattern work/JGroups-$1.GA.bin/[artifact].[ext] -deliverto work/JGroups-[revision].xml
+java -jar ../build/lib.ivy/ivy-2.0.0-rc2.jar -settings ivy.settings.xml -ivy install-jgroups-$1.xml -publish internal -revision $1 -publishpattern work/JGroups-$1.GA.bin/[artifact].[ext] -deliverto work/JGroups-[revision].xml
