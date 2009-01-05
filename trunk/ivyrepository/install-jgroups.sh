d273bc1e56e5f54cd4e5c96d2b717701750cd51e
takao.nakaguchi <takao.nakaguchi@cf2c226f-224e-0410-9fc3-354f046c9d04>
2009-01-05 18:24:47 +0000

    jgroupsインストールスクリプトとwhitedog、jgroupsライブラリ追加。

---
+++ trunk/ivyrepository/install-jgroups.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+	echo "Usage: install-jgroups.sh revision"
+	exit 1
+fi
+java -jar ../build/lib.ivy/ivy-2.0.0-rc2.jar -verbose -settings ivysettings.xml -ivy install-jgroups-$1.xml -publish internal -revision $1 -publishpattern work/JGroups-$1.GA.bin/[artifact].[ext] -deliverto work/JGroups-[revision].xml
