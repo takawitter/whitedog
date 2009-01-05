5ead190acdb437c0ca0b48e86fb568c24dd30f22
takao.nakaguchi <takao.nakaguchi@cf2c226f-224e-0410-9fc3-354f046c9d04>
2009-01-05 19:31:39 +0000

    change @Share documented

--- whitedog/src/main/java/jp/whitedog/annotation/Share.java
+++ whitedog/src/main/java/jp/whitedog/annotation/Share.java
@@ -16,13 +16,18 @@
  */
 package jp.whitedog.annotation;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Indicates that a method execution should be shared in session.
  * @author Takao Nakaguchi
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
 public @interface Share {
 }
