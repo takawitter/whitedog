48698aa679ee7077c3ec87ca7a160b6622a52a4d
takao.nakaguchi <takao.nakaguchi@cf2c226f-224e-0410-9fc3-354f046c9d04>
2009-01-05 18:46:50 +0000

    remove dependency of jgroups to log4j

--- trunk/ivyrepository/install-jgroups.sh
+++ trunk/ivyrepository/install-jgroups.sh
@@ -3,4 +3,4 @@ if [ $# -ne 1 ]; then
 	echo "Usage: install-jgroups.sh revision"
 	exit 1
 fi
-java -jar ../build/lib.ivy/ivy-2.0.0-rc2.jar -verbose -settings ivysettings.xml -ivy install-jgroups-$1.xml -publish internal -revision $1 -publishpattern work/JGroups-$1.GA.bin/[artifact].[ext] -deliverto work/JGroups-[revision].xml
+java -jar ../build/lib.ivy/ivy-2.0.0-rc2.jar -settings ivysettings.xml -ivy install-jgroups-$1.xml -publish internal -revision $1 -publishpattern work/JGroups-$1.GA.bin/[artifact].[ext] -deliverto work/JGroups-[revision].xml
